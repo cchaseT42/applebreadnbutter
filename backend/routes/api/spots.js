be376ff2aaad5860ebcd1a798203e9101c115d90
shoo <supershoo3@gmail.com>
2022-10-26 12:50:05 -0500

    Added more end points.

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,13 +1,51 @@
 const express = require('express');
 const { json } = require('sequelize');
+const Sequelize = require('sequelize');
+const op = Sequelize.Op;
 const { restoreUser, requireAuth } = require('../../utils/auth');
-const { Spot, SpotImage, Review, Sequelize, User } = require('../../db/models');
+const { Spot, SpotImage, Review, User } = require('../../db/models');
+const spot = require('../../db/models/spot');
 
 const router = express.Router();
 
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 router.get('/', async (req, res, next) => {
-  const spots = await Spot.findAll()
+  let { page, size, minLat, maxLat, minLng, maxLng, minPrice, maxPrice } = req.query;
+  page = parseInt(page);
+  size = parseInt(size);
+  minLat = parseInt(minLat);
+  maxLat = parseInt(maxLat);
+  minLng = parseInt(minLng);
+  maxLng = parseInt(maxLng);
+  minPrice = parseInt(minPrice);
+  maxPrice = parseInt(maxPrice);
+
+  if (Number.isNaN(page) || page <= 0) page = 1;
+  if (Number.isNaN(size) || size <= 0) size = 20;
+  if (Number.isNaN(minLat) || minLat < -90) minLat = -90
+  if (Number.isNaN(maxLat) || maxLat > 90) maxLat = 90
+  if (Number.isNaN(minLng) || minLng < -180) minLng = -180
+  if (Number.isNaN(maxLng) || maxLng > 180) maxLng = 180
+  if (Number.isNaN(minPrice) || minPrice < 0) minPrice = 0
+  if (Number.isNaN(maxPrice)) maxPrice = 100000000
+
+  const spots = await Spot.findAll({
+    where: {
+      lat: {
+        [op.between]: [minLat, maxLat]
+      },
+      lng: {
+        [op.between]: [minLng, maxLng]
+      },
+      price: {
+        [op.between]: [minPrice, maxPrice]
+      }
+    },
+    limit: size,
+    offset: size * (page - 1)
+  })
   let updatedSpots = []
   for (let i = 0; i < spots.length; i++){
     let spot = spots[i].toJSON();
@@ -34,7 +72,29 @@ router.get('/', async (req, res, next) => {
     updatedSpots.push(spot)
 
   }
-  res.json({'Spots': updatedSpots})
+  res.json({'Spots': updatedSpots}, {'page': page}, {'size': size})
+})
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+router.post('/', requireAuth, async (req, res) => {
+  const { user } = req;
+  const id = user.id
+  const { address, city, state, country, lat, lng, name, description, price} = req.body
+
+  let spot = await Spot.create({
+    ownerId: id,
+    address,
+    city,
+    state,
+    country,
+    lat,
+    lng,
+    name,
+    description,
+    price
+  })
+
+  res.json(spot)
 })
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -76,7 +136,44 @@ router.get('/current', restoreUser, async (req, res) => {
   }
   res.json({'Spots': updatedSpots})
 })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+router.post('/:spotId/images', requireAuth, async (req, res) => {
+  const { user } = req;
+  const id = user.id
+
+  const {url, preview} = req.body
+
+  let spot = await Spot.findByPk(req.params.spotId)
+  if (!spot){
+    const err = new Error('Search failed');
+    err.message = "Spot couldn't be found.";
+    res.statusCode = '404';
+    return res.json({
+      message: err.message,
+      statusCode: 404
+    })
+  }
+
+  if (spot.ownerId !== id) {
+    const err = new Error('Failed')
+    err.message = "You are not authorized to do that."
+    res.statsCode = '403'
+    return res.json({
+      message: err.message,
+      statusCode: 404
+    })
+  }
+
+  const img = await SpotImage.create({
+    spotId: spot.id,
+    url,
+    preview
+  })
+
+  return res.json(img)
+
+})
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 router.put('/:spotId', requireAuth, async (req, res) =>{
   const { user } = req;
@@ -94,6 +191,7 @@ router.put('/:spotId', requireAuth, async (req, res) =>{
     })
 }
   if (spot.ownerId !== id){
+    console.log(spot.ownerId, id)
     const err = new Error('Search failed');
     err.message = "You are not authorized to do that.";
     res.statusCode = '403';
@@ -103,7 +201,7 @@ router.put('/:spotId', requireAuth, async (req, res) =>{
     })
   }
   console.log(spot)
-  spot.set({
+  spot.update({
     address,
     city,
     state,
@@ -114,6 +212,8 @@ router.put('/:spotId', requireAuth, async (req, res) =>{
     description,
     price
   })
+
+  await spot.validate()
 
   await spot.save
   res.json(spot)
