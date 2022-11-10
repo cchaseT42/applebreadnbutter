0691f63d293945984caa6632b830a3fcf981acd5
shoo <supershoo3@gmail.com>
2022-11-10 16:14:37 -0600

    Finished AuthMe Frontend phase 0

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
-const store = configureStore();
+// ... const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
