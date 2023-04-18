9d11c80b08beb1fa35613688a32695bc8fe5a0a2
shoo <supershoo3@gmail.com>
2023-04-18 13:26:41 -0500

    fixed backend issue

--- backend/db/migrations/20221024173939-create-review.js
+++ backend/db/migrations/20221024173939-create-review.js
@@ -32,7 +32,7 @@ module.exports = {
         onDelete: 'cascade'
       },
       review: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(3000)
       },
       stars: {
         type: Sequelize.INTEGER
